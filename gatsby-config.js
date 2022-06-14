8d149c46ba05414acccdbf6a88c11611c1984311
Suélen Dias Palhares <suelenddf@gmail.com>
2022-06-14 18:23:57 -0300

    Add Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,41 @@
  */
 
 module.exports = {
+  pathPrefix: "Hiring-Coders-GatsbyJS",
   siteMetadata: {
-    title: `Gatsby`,
+    siteUrl: 'https://suelendp.github.io/Hiring-Coders-GatsbyJS/',
+    title: `Hiring Coders #3`,
+    description:'Aplicando os conhecimentos de criação de site utilizando o GatsbyJS',
   },
   plugins: [
     `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/assets/icon.png',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`foo-bar.js`],
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
+import * as headerStyles from '../styles/header.module.scss';
 
 
 
@@ -7,8 +8,10 @@ import Layout from "../components/layout"
 const Index = () =>{
   return (
     <Layout>
+      <container  className={headerStyles.body}>
       <h1>Home Page</h1>
       <h2>Aula GatsbyJS</h2>
+      </container>
     </Layout>
 
 
